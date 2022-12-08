46116abeaeb0f4397c744d64d5b9f0ae494df2a1
SeunghaeC <119824334+SeunghaeC@users.noreply.github.com>
2022-12-08 22:19:56 +0900

    Update Order.java
    update_20221208

--- client/src/main/java/shfooddelivery/domain/Order.java
+++ client/src/main/java/shfooddelivery/domain/Order.java
@@ -1,8 +1,6 @@
 package shfooddelivery.domain;
 
-import shfooddelivery.domain.주문됨;
 import shfooddelivery.domain.OrderPlaced;
-import shfooddelivery.domain.주문취소됨;
 import shfooddelivery.domain.OrderCancled;
 import shfooddelivery.ClientApplication;
 import javax.persistence.*;
@@ -55,43 +53,13 @@ public class Order  {
     @PostPersist
     public void onPostPersist(){
 
-        //Following code causes dependency to external APIs
-        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
-
-
-        shfooddelivery.external.결제이력 결제이력 = new shfooddelivery.external.결제이력();
-        // mappings goes here
-        Application.applicationContext.getBean(shfooddelivery.external.결제이력Service.class)
-            .결제(결제이력);
-
-
-        주문됨 주문됨 = new 주문됨(this);
-        주문됨.publishAfterCommit();
-
-
-        //Following code causes dependency to external APIs
-        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
-
-
-        shfooddelivery.external.결제이력 결제이력 = new shfooddelivery.external.결제이력();
-        // mappings goes here
-        Application.applicationContext.getBean(shfooddelivery.external.결제이력Service.class)
-            .결제(결제이력);
-
-
         OrderPlaced orderPlaced = new OrderPlaced(this);
         orderPlaced.publishAfterCommit();
 
     }
     @PrePersist
     public void onPrePersist(){
-
-
-        주문취소됨 주문취소됨 = new 주문취소됨(this);
-        주문취소됨.publishAfterCommit();
-
-
-
+        
         OrderCancled orderCancled = new OrderCancled(this);
         orderCancled.publishAfterCommit();
 
